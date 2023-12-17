486f117ae788bf95ca391cec62cf74223f246957
fosfito <fosfito@gmail.com>
2023-12-17 19:32:02 -0300

    1.b sin ganas

--- 01b.js
+++ 01b.js
@@ -3,33 +3,35 @@ const cuenta1 = {
     tipo: "corriente", //"ahorro"
     importe: 200,
     tipomodena: "Peso",
-    plazofijo: [{estado:"Activo",importe:100,fechaCreacion:"2015-01-01",plazo:38}]
+    plazofijo: [{estado:"activo",importe:100,fechaCreacion:"2015-01-01",plazo:38}]
 }
 
 const cuenta2 = {
     tipo: "ahorro", //"ahorro"
     importe: 23000,
     tipomodena: "Peso",
-    plazofijo: [{estado:"Activo",importe:100,fechaCreacion:"2020-01-01",plazo:30}]
+    plazofijo: [{estado:"activo",importe:100,fechaCreacion:"2020-01-01",plazo:32},
+                {estado:"inactivo",importe:100,fechaCreacion:"2020-01-01",plazo:35},
+                {estado:"activo",importe:100,fechaCreacion:"2020-01-01",plazo:20}]
 }
 const cuenta3 = {
     tipo: "ahorro", //"ahorro"
     importe: 200,
     tipomodena: "dolar",
-    plazofijo: [{estado:"Activo",importe:100,fechaCreacion:"2023-01-01",plazo:30}]
+    plazofijo: [{estado:"inactivo",importe:100,fechaCreacion:"2023-01-01",plazo:30}]
 }
 
 const cuenta4 = {
     tipo: "corriente", //"ahorro"
     importe: 200,
     tipomodena: "Peso",
-    plazofijo: [{estado:"activo",importe:100,fechaCreacion:"2019-01-01",plazo:32}]
+    plazofijo: [{estado:"inactivo",importe:100,fechaCreacion:"2019-01-01",plazo:32}]
 }
 const cuenta5 = {
     tipo: "corriente", //"ahorro"
     importe: 200,
     tipomodena: "Peso",
-    plazofijo: [{estado:"Activo",importe:100,fechaCreacion:"2020-01-01",plazo:30}]
+    plazofijo: [{estado:"inactivo",importe:100,fechaCreacion:"2020-01-01",plazo:30}]
 }
 
 const cuenta6 = {
@@ -45,13 +47,18 @@ const cuenta=[cuenta1, cuenta2, cuenta3, cuenta4, cuenta5, cuenta6];
 
 function mayores30d(cuentas) 
     {
-        console.log("Cuentas con plazos activos y mayores que 30");
+        console.log("Plazos fijos mayores a 30");
         for (let i = 0; i < cuentas.length; i++) 
         {
-           // if (cuentas[i].plazofijo[2]>30)
+           const cuentaUsuario=cuentas[i];
+           for (let j = 0; j < cuentaUsuario.plazofijo.length; j++) 
             {
-            console.log(cuentas[i].plazofijo.estado)    
-            }            
+             if ((cuentaUsuario.plazofijo[j].estado=="activo")&&(cuentaUsuario.plazofijo[j].plazo>30))
+                {
+                console.log(cuentaUsuario);
+                break;   //ya encontro 1 ese usuario sale registrado
+                }   
+            }
         }
     }
 
