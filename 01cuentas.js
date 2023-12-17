4600c44a24fd4eddc5f0d20fcf5c4dabcc7cec13
fosfito <fosfito@gmail.com>
2023-12-17 16:39:29 -0300

    Sin ganas

---
+++ 01cuentas.js
@@ -0,0 +1,43 @@
+//6 Cuentas con datos
+
+cuenta1 = {
+    tipo: "corriente", //"ahorro"
+    importe: 200,
+    tipomodena: "Peso",
+    plazofijo: [{estado:"Activo",importe:100,fechaCreacion:"2015-01-01",plazo:30,}],
+}
+
+cuenta2 = {
+    tipo: "cahorro", //"ahorro"
+    importe: 23000,
+    tipomodena: "Peso",
+    plazofijo: [{estado:"Activo",importe:100,fechaCreacion:"2020-01-01",plazo:30,}],
+}
+cuenta3 = {
+    tipo: "ahorro", //"ahorro"
+    importe: 200,
+    tipomodena: "dolar",
+    plazofijo: [{estado:"Activo",importe:100,fechaCreacion:"2023-01-01",plazo:30,}],
+}
+
+cuenta4 = {
+    tipo: "corriente", //"ahorro"
+    importe: 200,
+    tipomodena: "Peso",
+    plazofijo: [{estado:"inactivo",importe:100,fechaCreacion:"2019-01-01",plazo:30,}],
+}
+cuenta5 = {
+    tipo: "corriente", //"ahorro"
+    importe: 200,
+    tipomodena: "Peso",
+    plazofijo: [{estado:"Activo",importe:100,fechaCreacion:"2020-01-01",plazo:30,}],
+}
+
+cuenta6 = {
+    tipo: "corriente", //"ahorro"
+    importe: 200,
+    tipomodena: "Peso",
+    plazofijo: [{estado:"inactivo",importe:500,fechaCreacion:"2025-01-01",plazo:30,}],
+}
+
+export const cuenta=[cuenta1, cuenta2, cuenta3, cuenta4, cuenta5, cuenta6];
