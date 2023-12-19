d18a47613f3f17e71d2c40f62d8f6202d4013949
fosfito <fosfito@gmail.com>
2023-12-19 10:02:16 -0300

    3.d

--- 03c.js
+++ 03c.js
@@ -1,51 +1,42 @@
 const butaca1 = {
-                numero: 1,
-                fila: "A",
-                estado: "ocupado", // "ocupada"
-                ubicacion: "centro" // "derecha" // "centro"
-                }
+    numero: 1,
+    fila: "A",
+    estado: "ocupada", // "ocupada"
+    ubicacion: "centro" // "derecha" // "centro"
+    }
 const butaca2 = {
-                    numero: 2,
-                    fila: "A",
-                    estado: "libre", // "ocupada"
-                    ubicacion: "centro" // "derecha" // "centro"
-                }
+        numero: 2,
+        fila: "A",
+        estado: "libre", // "ocupada"
+        ubicacion: "centro" // "derecha" // "centro"
+    }
 const butaca3 = {
-                    numero: 3,
-                    fila: "A",
-                    estado: "ocupado", // "ocupada"
-                    ubicacion: "izquierda"// "derecha" // "centro"
-                }
+        numero: 3,
+        fila: "A",
+        estado: "ocupada", // "ocupada"
+        ubicacion: "izquierda"// "derecha" // "centro"
+    }
 
 const butaca4 = {
-                    numero: 4,
-                    fila: "A",
-                    estado: "libre", // "ocupada"
-                    ubicacion: "izquierda" // "derecha" // "centro"
-                    }
+        numero: 4,
+        fila: "A",
+        estado: "ocupada", // "ocupada"
+        ubicacion: "izquierda" // "derecha" // "centro"
+        }
 const butacas=[butaca1,butaca2,butaca3,butaca4];
 
-//3b) Realiza una función que devuelva las butacas libres dada una cierta ubicación.
+//3c) Realiza una función que devuelva las butacas ocupadas
 
 function ocupadas(sillas)
 {
-    const rdos=[0,0,0];  //izquierda, centro, derecha
-    
-   
-    for (let i = 0; i < sillas.length; i++) 
-    {
-        if ((sillas[i].estado=="ocupado")&&(sillas[i].ubicacion=="izquierda"))rdos[0]++; 
-        if ((sillas[i].estado=="ocupado")&&(sillas[i].ubicacion=="centro"))rdos[1]++;
-        if ((sillas[i].estado=="ocupado")&&(sillas[i].ubicacion=="derecha"))rdos[2]++; 
-    }
-    if ((rdos[0]>rdos[1])&&(rdos[0]>rdos[2])){rta="Izquierda"}
-    if ((rdos[0]==rdos[2])&&(rdos[0]>rdos[1])){rta="Izquierda y derecha"}
-    if ((rdos[0]==rdos[1])&&(rdos[0]>rdos[2])){rta="Izquierda y centro"}
-    if ((rdos[1]>rdos[0])&&(rdos[1]>rdos[2])){rta="centro"}
-    if ((rdos[2]>rdos[1])&&(rdos[2]>rdos[0])){rta="derecha"}
-    if ((rdos[2]==rdos[1])&&(rdos[2]>rdos[0])){rta="centro y derecha"}
-    if ((rdos[2]==rdos[1])&&(rdos[2]==rdos[0])){rta="Homogeneo"}
-    console.log("sillas:"+rta+" Rtas: "+rdos);
-    return(rta)
+let contador=0;
+for (let i = 0; i < sillas.length; i++) 
+{
+if(sillas[i].estado=="ocupada")
+{
+contador++;
 }
-console.log("La mayoria vendida esta en:"+ocupadas(butacas));
+}
+return(contador)
+}
+console.log("Las butacas ocupadas son:"+ocupadas(butacas));
